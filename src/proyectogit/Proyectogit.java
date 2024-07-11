b1cd68846561d18b8735d2885412d3cbeacd5522
Alison Mejía <alison.mejia28@outlook.com>
2024-07-11 12:06:15 -0600

    ejercicio

---
+++ src/proyectogit/Proyectogit.java
@@ -0,0 +1,16 @@
+
+package proyectogit;
+
+
+public class Proyectogit {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Tabla de multipicar");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(10 + "*" + i + "=" + 10*i);
+        }
+        
+    }
+    
+}
